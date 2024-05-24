254d82baa2a9ccbfb7b4303c003a0b143d4abb62
Thiago <santos.td@gmail.com>
2024-05-24 12:04:40 -0300

    NEW: a simple firedrake script to run the Darcy flow in the reservoir (no wellbore hydrodynamics)

---
+++ src/runme.py
@@ -0,0 +1,29 @@
+from firedrake import *
+import ufl
+import numpy as np
+import math
+import time as tm
+import sys
+
+meshfile = "/home/thiago/Documents/Postdoc_ITA/mesh_generation/geo/mesh3D_rev01.msh"
+mesh = Mesh(meshfile)
+V = FunctionSpace(mesh, "CG",1)
+
+u = TrialFunction(V)
+v = TestFunction(V)
+p = Function(V)
+
+F = inner(grad(u),grad(v))*dx + Constant(0)*v*dx
+
+Pr=10;
+Pw=1;
+p_farfield = DirichletBC(V, Pr, [100])
+p_well = DirichletBC(V, Pw, [103])
+
+_lhs = lhs(F)
+_rhs = rhs(F)
+problem = LinearVariationalProblem(_lhs, _rhs, p, bcs=[p_farfield,p_well])
+solver  = LinearVariationalSolver(problem)
+solver.solve()
+
+File("p.pvd").write(p)
