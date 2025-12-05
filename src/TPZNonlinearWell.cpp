e4ee58ca9092bb98793fa128e8fe038473978524
g-taraschi <gitaraschi@gmail.com>
2025-12-05 09:51:28 -0300

    fix: correct the implementation of reynolds number

--- src/TPZNonlinearWell.cpp
+++ src/TPZNonlinearWell.cpp
@@ -76,10 +76,9 @@ void TPZNonlinearWell::Contribute(const TPZVec<TPZMaterialDataT<STATE>> &datavec
     REAL psol = datavec[1].sol[0][0];
     REAL divQsol = datavec[0].divsol[0][0];
 
-    REAL reynolds = (fRho * std::abs(Qsol) * fDw) / fMu;
+    REAL velocity = 4. * Qsol / (M_PI * fDw * fDw);
+    REAL reynolds = (fRho * std::abs(velocity) * fDw) / fMu;
     bool turbulent = reynolds > 1187.38;
-    // turbulent = reynolds > 0.1; // for testing purposes
-
     // Tangent matrix
     REAL factor = turbulent ? fC * weight * (pow(std::abs(Qsol), 3. / 4.) + 3. / 4. * pow(std::abs(Qsol), 3. / 4.)) : fCLin * weight;
     ek.AddContribution(0, 0, phiQ, 0, phiQ, 1, factor);      // A
